b76b5670a68182f986adfeb6a86f6a6614ca2a5c
Janaligopang <113940976+Janaligopang@users.noreply.github.com>
2022-09-21 14:23:08 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['923183098772']
+global.premium = ['923183098772']
+global.ownernomer = '923183098772'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
